3a88c12e8a4484205d25300ddac2cf1e61695e8f
Omar Aleiser Alvarez Laverde <omaraleyser@hotmail.com>
2022-10-16 20:44:21 -0500

    finalizado

--- temp.js
+++ temp.js
@@ -8,7 +8,7 @@ function iniciar() {
 function contador(inicial) {
   if (inicial <= 0) {
     document.getElementById("tiempo").innerText = "Finalizado";
-    'tiempo'.style.color= 'red'
+    inical.style.color= 'red'
     
   } else {
     document.getElementById("tiempo").innerText = inicial;
